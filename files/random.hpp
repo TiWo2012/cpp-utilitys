f1122f179306c25907ed53e8d51cd7a1a4de044c
= <=>
2024-08-15 13:51:18 +0200

    added somme stuf

--- files/random.hpp
+++ files/random.hpp
@@ -1,10 +1,18 @@
 #pragma once
-namespace lib {
-  Template<T>
-  T random(long long seed, T num, T range)
-  {
-    T bet = seed * num + num / num - num;
-    T res = static_cast<T>(bet)
-    return res * range;
-  }
+namespace lib
+{
+    //pseudo_random generates an random number in any datatype based on an seed that you type in
+    Template<T>
+    T pseudo_random(long long seed, T num, T range)
+    {
+        T bet = seed * num + num / num - num;
+        T res = static_cast<T>(bet)
+        return res * range;
+    }
+
+    //the number 4 is completly random chosen by me
+    int rand_int()
+    {
+        return 4;
+    }
 }
