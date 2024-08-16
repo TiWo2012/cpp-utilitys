8512a476174bfad9517edded251a1c3d0b7f286c
TiWo2012 <timo.wolf@gmx.ch>
2024-08-16 20:27:39 +0000

    fixed bugs

--- files/random.hpp
+++ files/random.hpp
@@ -6,7 +6,7 @@ namespace scl
     long long pseudo_random(long long seed, long long num, long long range)
     {
         long long bet = seed * num + num / num - num;
-        long long res = static_cast<long long>(bet)
+        long long res = static_cast<long long>(bet);
         return res * range;
     }
 
