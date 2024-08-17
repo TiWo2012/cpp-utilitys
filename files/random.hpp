a0f46bf3c4bbc5e2ecdc5d199b64deb573e2f04b
TiWo2012 <timo.wolf@gmx.ch>
2024-08-17 23:49:49 +0200

    changed the pseudo random functuon becorse unused code

--- files/random.hpp
+++ files/random.hpp
@@ -10,8 +10,7 @@ namespace scl
     long long pseudo_random(long long seed, long long num, long long range)
     {
         long long bet = seed * num + num / num - num;
-        long long res = static_cast<long long>(bet);
-        return res * range;
+        return bet * range;
     }
 
     //the number 4 is completly random chosen by me
