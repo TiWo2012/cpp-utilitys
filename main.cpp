dbec9c8d69fab1982f6fa732629048541e941f45
TiWo2012 <timo.wolf@gmx.ch>
2024-08-17 21:13:13 +0000

    started writing tests for all files but not completly finished

--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
 
+// hpp files
+#include "files/random.hpp"
+#include "files/console.hpp"
+
 int main()
 {
+    // random
+    // pseudo random
+    long long test1 = scl::pseudo_random(2,2,2);
+
+    if (test1 == 2)
+    {
+        std::cout << "test1 passed";
+    }
+    else
+    {
+        return 1;
+    }
+
+    // rand int
+    int test2 = scl::rand_int();
+
+    if (test2 == 4)
+    {
+        std::cout << "test2 passed";
+    }
+    else
+    {
+        return 1;
+    }
+
+    // rand float
+    float test3 = scl::rand_float();
+
+    if (test3 == 0.3221F)
+    {
+        std::cout << "test3 passed";
+    }
+    else
+    {
+        return 1;
+    }
+
+
+    // console
+    // clear
+
+    // write
+    
+    //write and clear
+
+    // print multiline
+
+    // print multiline and clear
+
+    
     return 0;
 }
